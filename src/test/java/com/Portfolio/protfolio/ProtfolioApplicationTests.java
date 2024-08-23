91cd0dfa356098b47ed479cbed757132fd37ba8c
rkdalswo0328 <rkdalswo0328@gmail.com>
2024-08-23 14:37:06 +0900

    first commit

---
+++ src/test/java/com/Portfolio/protfolio/ProtfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Portfolio.protfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProtfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
